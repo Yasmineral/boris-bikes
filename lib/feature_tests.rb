915c03acf31450b237ce8f72b1080850cf85f851
Jessie <lonsdale.jess@gmail.com>
2019-09-20 13:45:02 +0100

    middle of ch20

--- lib/feature_tests.rb
+++ lib/feature_tests.rb
@@ -4,5 +4,5 @@ require_relative 'docking_station'
 station = DockingStation.new
 my_bike = Bike.new
 my_bike.report_broken
+
 station.dock(my_bike)
-p station.release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,9 +8,12 @@ describe DockingStation do
 
   it 'releases working bikes from occupied stations' do
     docking_station = DockingStation.new
-    docking_station.dock(Bike.new)
-    bike = docking_station.release_bike
-    expect(bike).to be_working
+
+    double_bike = double("bike")
+    allow(double_bike).to receive(:working?).and_return(true)
+
+    docking_station.dock(double_bike)
+    expect(double_bike.working?).to eq true
   end
 
   it 'responds to dock with one argument' do
@@ -19,20 +22,20 @@ describe DockingStation do
 
 it { is_expected.to respond_to(:bikes)}
 
-it 'returns docked bikes' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bikes).to eq [bike]
-end
+#it 'returns docked bikes' do
+  #double_bike = double("bike")
+  #allow(double_bike).to receive(:working?).and_return(true)
+  #expect(subject.bikes).to eq [bike]
+#end
 
 it 'raises an error when there are no available bikes' do
-  docking_station = DockingStation.new 
+  docking_station = DockingStation.new
   expect {docking_station.release_bike}.to raise_error("NoBikeError")
 end
- 
+
 it 'raises an error when there is a bike already docked' do
   ds = DockingStation.new
-  DockingStation::DEFAULT_CAPACITY.times {ds.dock(Bike.new)}
+  DockingStation::DEFAULT_CAPACITY.times {ds.dock(double(:bike))}
   expect {ds.dock(Bike.new)}.to raise_error("TooManyBike")
 end
 
@@ -40,11 +43,10 @@ it {expect(DockingStation.new(5).capacity).to eq 5}
 it {expect(DockingStation.new.capacity).to eq 20}
 
 it "doesn't release a bike if it is broken" do
-  my_bike = Bike.new
+  my_bike = double(:bike)
   my_bike.report_broken
   subject.dock(my_bike)
-  expect{subject.release_bike}.to raise_error("FaultyBikeError") 
+  expect{subject.release_bike}.to raise_error("FaultyBikeError")
 end
 
-end 
-
+end
